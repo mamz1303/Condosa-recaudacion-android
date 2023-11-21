975ddd65c91fe1231cc2a25ad615f7754dfbe86d
JeffersonUnmsm10 <143053248+JeffersonUnmsm10@users.noreply.github.com>
2023-11-21 01:55:24 -0500

    Informacion del Personal Serializable

--- app/src/main/java/com/example/recaudacion/network/InformacionPersonal.kt
+++ app/src/main/java/com/example/recaudacion/network/InformacionPersonal.kt
@@ -4,7 +4,9 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 
-
+// Cada campo tiene una anotación @SerialName que permite especificar un nombre específico que
+// se utilizará al serializar/deserializar el objeto, lo que puede ser útil si los nombres 
+// de los campos difieren en la representación serializada.
 
 /* Información personal */
 
